b4a28760a717abb10ec8a3b2b542985f5fc97956
omowole daniel <omowoledanielsanmi@gmail.com>
2023-09-04 22:11:15 +0100

    resumeUpdate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -129,7 +129,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/14GBhhgXbYFsL0W7f0W8t4_0dKagDwpLh/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1nM7SIUgTWj8GZl2isS6TQN9xpPThIVzT/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
