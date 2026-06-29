0e6465ae620e77176961829a8a2da011dceeea7a
Daniel Omowole <101469475+Sanmidaniels07@users.noreply.github.com>
2026-06-29 15:22:50 +0100

    changes-on-work

--- src/data/data.js
+++ src/data/data.js
@@ -5,6 +5,8 @@ import cis from '../assets/projects/cis.jpg'
 import evea from '../assets/projects/evea.jpg'
 import eveaImage from '../assets/projects/evea-app.jpg'
 import cipmnAdmin from '../assets/projects/cipmn-admin-app.jpg'
+import qucikmed from '../assets/projects/quickmed.jpg'
+import eveaWebsite from '../assets/projects/evea-website.jpg'
 
 
 export const data=[
@@ -14,7 +16,7 @@ export const data=[
         name:"Evea Cash and Credit Management",
         image:eveaImage,
         github:"https://github.com/Evea01/evea-app-v2",
-        live:"https://www.evea.africa/",
+        live:"https://accounts.evea.africa/",
     },
      {
         id:2,
@@ -30,8 +32,23 @@ export const data=[
         github:"https://github.com/Sanmidaniels07/TPS-wEB.git",
         live:"https://cisltd.com/",
     },
-    {
+     {
         id:4,
+        name:"Evea",
+        image:evea,
+        github:"https://github.com/Evea01/Frontend",
+        live:"https://evea-dev.evea.africa/",
+    },
+
+    {
+        id:5,
+        name:"QuickMed",
+        image:qucikmed,
+        github:"",
+        live:"https://www.quickmedng.com/",
+    },
+    {
+        id:6,
         name:"Gpt3 Application",
         image:AI,
         github:"https://github.com/Sanmidaniels07/GPT3-webApp",
@@ -39,7 +56,7 @@ export const data=[
     },
 
     {
-        id:5,
+        id:7,
         name:"Responsive Admin Dashboard",
         image:AdminDashboard,
         github:"https://github.com/Sanmidaniels07/Admin-Dashboard.git",
@@ -48,21 +65,20 @@ export const data=[
 
    
     {
-        id:6,
+        id:8,
         name:"Business Application",
         image:business,
         github:"https://github.com/Sanmidaniels07/CourseList-withZustand",
         live:"https://hoobank-business-app.netlify.app/",
     },
-
-    {
-        id:7,
-        name:"Evea",
-        image:evea,
-        github:"https://github.com/Evea01/Frontend",
-        live:"https://evea-dev.evea.africa/",
+ 
+{
+        id:9,
+        name:"Evea Website",
+        image:eveaWebsite,
+        github:"https://github.com/Evea01/evea-website-v2",
+        live:"https://www.evea.africa/",
     },
-
 
 
 
